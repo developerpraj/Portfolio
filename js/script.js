84f8422cbdf7611bfcac2851c4dd5e25b2aca360
developerpraj <56839366+developerpraj@users.noreply.github.com>
2019-11-10 22:57:08 -0600

    Bootstraped md-5 and md-7 section for about me..

--- js/script.js
+++ js/script.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
     play: 5000,
     pagination:false
   });
+
+
+  var typed = new Typed(".typed",{
+    strings:["Software Engineer.","Java Developer.","Student.","Web Developer."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+
+  });
+
 });
