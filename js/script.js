710a749240ff9d687eb09654b14b4efd0090f0c9
developerPraj <prajjwol52@gmail.com>
2020-05-18 14:16:40 -0500

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+
+$(window).on("load",function(){
+  $(".loader .inner").fadeOut(500 ,function(){
+    $(".loader").fadeOut(750);
+  });
+})
+
+
+
+
+
+
 $(document).ready(function(){
   $('#slides').superslides({
     animation: 'slide',
@@ -7,7 +19,7 @@ $(document).ready(function(){
 
 
   var typed = new Typed(".typed",{
-    strings:["Software Engineer.","Java Developer.","Student.","Web Developer."],
+    strings:["Welcome","I am a","Software Engineer.",".NET Developer.","Writer","Blogger","and a lifleong Student."],
     typeSpeed: 70,
     loop: true,
     startDelay: 1000,
@@ -61,4 +73,28 @@ $(document).ready(function(){
                   countUpFinished=true;
             }
           });
+
+           $("#boss li a").click(function(e){
+             e.preventDefault();
+             var targetElement = $(this).attr("href");
+             var targetPosition =$(targetElement).offset().top;
+             $("html, body").animate({scrollTop:targetPosition -50 },"slow")
+           });
+
+
+          const nav =$("#boss");
+          const navTop=nav.offset().top;
+
+          $(window).on("scroll",stickyNavigation);
+          function stickyNavigation(){
+            var body =$("body");
+            if ($(window).scrollTop() >= navTop){
+              body.css("padding.top",nav.outerHeight() + "px");
+              body.addClass("fixedNav");
+            }
+            else {
+                body.css("padding.top",0);
+              body.removeClass("fixedNav");
+            }
+          }
 });
